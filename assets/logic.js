b955674eb24259f4393bda76a280cc1a711bf944
tbsdvl <tristonburns97@gmail.com>
2021-03-11 14:07:29 -0700

    Linking api data to HTML elements.

--- assets/logic.js
+++ assets/logic.js
@@ -2,7 +2,18 @@
 // API key - adfd291a73cb793a96b747e8e6006e24
 // var openWeatherAPI = "https://api.openweathermap.org/data/2.5/weather?q=Tucson&appid=adfd291a73cb793a96b747e8e6006e24";
 
+var city = document.getElementById("cityheader");
+var date = document.querySelector("#date");
+var temp = document.querySelector("#temp");
+var humid = document.querySelector("#humidity");
+var wind = document.querySelector("#windspeed");
+var uvi = document.querySelector("#uvi")
+var uviText = document.querySelector("#uvitext");
+var weatherType = document.querySelector("#icon");
+
 function getAPI(){
+
+    // will present historical data. Need to collect current date information
 apiURL = "https://api.openweathermap.org/data/2.5/forecast?q=";
 citySearch = document.querySelector("#city").value;
 apiKey = "&appid=adfd291a73cb793a96b747e8e6006e24";
@@ -17,19 +28,27 @@ apiURL = apiURL + citySearch + tempUnits + apiKey;
             // reference to data
             console.log(data)
 
-            for(i = 0; i <= 5; i++){
+            
             // get city name and the date
-            console.log(data["city"]["name"]);
-            console.log(data["list"][i]["dt_txt"])
+            cityName = (data["city"]["name"]);
+            city.innerHTML = "City: " + cityName;
+            // for(i = 0; i <= 5; i++){
+            currentDate = moment((data["list"][0]["dt"])).format("MMM Do, YYYY");
+            date.innerHTML = "Date: " + currentDate;
 
             // get weather conditions, weather icon, wind speed
-            console.log(data["list"][i]["weather"][0]["main"]);
-            console.log(data["list"][i]["weather"][0]["icon"]);
-            console.log(data["list"][i]["wind"]["speed"]);
+            weatherConditions = (data["list"][0]["weather"][0]["main"]);
+            weatherIcon = (data["list"][0]["weather"][0]["icon"]);
+            windSpeed = (data["list"][0]["wind"]["speed"]);
+            wind.innerHTML = "Wind Speed: " + windSpeed + " MPH";
+            weatherType.setAttribute("src", "http://openweathermap.org/img/wn/" + weatherIcon + ".png");
+            weatherType.setAttribute("alt", "Weather Icon");
 
             // get temp + humidity
-            console.log(data["list"][i]["main"]["temp"]);
-            console.log(data["list"][i]["main"]["humidity"]);
+            temperature = (data["list"][0]["main"]["temp"]);
+            temp.innerHTML = "Temperature: " + temperature + " °F";
+            humidity = (data["list"][0]["main"]["humidity"]);
+            humid.innerHTML = "Humidity: " + humidity + "%";
 
             // get UV index
             lattitude = (data["city"]["coord"]["lat"])
@@ -44,10 +63,11 @@ apiURL = apiURL + citySearch + tempUnits + apiKey;
                 })
                 .then(function (data){
                     for(i = 0; i <= 5; i++){
-                        console.log(data[i]["value"])
+                        uvIndex = data[i]["value"]
+                        uvi.textContent = "UV Index: " + uvIndex;
                     }
                 })
-            }
+        // }
         });
 };
 

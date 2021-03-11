566eb3cb2952b70aaab62de016525eb3146b388d
tbsdvl <tristonburns97@gmail.com>
2021-03-11 00:22:45 -0700

    Adding api calls to fetch a city's weather information

--- assets/logic.js
+++ assets/logic.js
@@ -2,26 +2,52 @@
 // API key - adfd291a73cb793a96b747e8e6006e24
 // var openWeatherAPI = "https://api.openweathermap.org/data/2.5/weather?q=Tucson&appid=adfd291a73cb793a96b747e8e6006e24";
 
-// fetch(openWeatherAPI)
-//     .then(function (response){
-//         return response.json();
-//     })
-//     .then(function (data){
-//         console.log(data);
-//     });
-
 function getAPI(){
-apiURL = "https://api.openweathermap.org/data/2.5/weather?q=";
+apiURL = "https://api.openweathermap.org/data/2.5/forecast?q=";
 citySearch = document.querySelector("#city").value;
 apiKey = "&appid=adfd291a73cb793a96b747e8e6006e24";
-apiURL = apiURL + citySearch + apiKey;
+tempUnits = "&units=imperial"
+apiURL = apiURL + citySearch + tempUnits + apiKey;
 
     fetch(apiURL)
         .then(function (response){
             return response.json();
         })
         .then(function (data){
-            console.log(data);
+            // reference to data
+            console.log(data)
+
+            for(i = 0; i <= 5; i++){
+            // get city name and the date
+            console.log(data["city"]["name"]);
+            console.log(data["list"][i]["dt_txt"])
+
+            // get weather conditions, weather icon, wind speed
+            console.log(data["list"][i]["weather"][0]["main"]);
+            console.log(data["list"][i]["weather"][0]["icon"]);
+            console.log(data["list"][i]["wind"]["speed"]);
+
+            // get temp + humidity
+            console.log(data["list"][i]["main"]["temp"]);
+            console.log(data["list"][i]["main"]["humidity"]);
+
+            // get UV index
+            lattitude = (data["city"]["coord"]["lat"])
+            longitude = (data["city"]["coord"]["lon"])
+            lonPrefix = "&lon="
+            apiURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat="
+            apiURL = apiURL + lattitude + lonPrefix + longitude + apiKey;
+
+            fetch(apiURL)
+                .then(function (response){
+                    return response.json();
+                })
+                .then(function (data){
+                    for(i = 0; i <= 5; i++){
+                        console.log(data[i]["value"])
+                    }
+                })
+            }
         });
 };
 
@@ -29,3 +55,4 @@ var submitCity = document.querySelector("#submit");
 submitCity.addEventListener("click", function(){
     getAPI();
 });
+
